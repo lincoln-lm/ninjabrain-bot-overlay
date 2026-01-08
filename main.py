0658767f9a256f7570af714113ad98161cb8c31c
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2026-01-07 20:42:59 -0700

    [style] clean

--- main.py
+++ main.py
@@ -12,21 +12,8 @@ async def sse_proxy(request: Request):
     print(f"{NINJABRAIN_BOT_URL}{request.url.path}")
 
     async def event_stream():
-        print("starting event stream")
         async for event in aiosseclient(f"{NINJABRAIN_BOT_URL}{request.url.path}"):
-            print("got event")
-            print(event)
             yield f"data: {event}\n\n"
-
-        # async with httpx.AsyncClient(timeout=None) as client:
-        #     async with client.stream(
-        #         "GET", f"{NINJABRAIN_BOT_URL}{request.url.path}"
-        #     ) as resp:
-        #         async for chunk in resp.aiter_bytes():
-        #             print("sending update")
-        #             yield f"data: {chunk.decode("utf-8")}\n\n"
-        #             if await request.is_disconnected():
-        #                 break
 
     headers = {
         "Cache-Control": "no-cache",
